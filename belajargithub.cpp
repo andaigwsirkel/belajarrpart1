249f571af43b9676ef03bdb901deee5d97152fd5
andaigwsirkel <abiyyuiqbalrawi@gmail.com>
2026-03-09 21:03:22 +0700

    ko-

---
+++ belajargithub.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string name, nrp;
+    cout << "Nama : Abiyyu Iqbal Rawi\n";
+    cout << "NRP : 2040251065\n";
+    return 0;
+
+}
