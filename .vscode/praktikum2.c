ccd79c4ab2256f3c9dd847172afe9644b1b30669
andaigwsirkel <abiyyuiqbalrawi@gmail.com>
2026-03-12 21:42:10 +0700

    ko-

---
+++ .vscode/praktikum2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main()
+{
+    printf(“selamat datang di vokasi....\n”);
+    printf(“kampus dengan sejuta praktikum.....”);
+    printf(“dan anda adalah mahasiswa angkatan %d”,2019);
+}
